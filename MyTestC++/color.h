fd7ab257b7c5866fbd3950c586d2042909d83987
maxim <mr.max1352@gmail.com>
2019-06-19 19:31:08 +0500

    Delete color.h

--- MyTestC++/color.h
+++
@@ -1,9 +0,0 @@
-#include "enum_helper_pre.h" 
-
-enumeration_begin(color)
-declare_member(RED) delimiter
-declare_member(GREEN) delimiter
-declare_member(BLUE)
-enumeration_end;
-
-#include "enum_helper_post.h"
